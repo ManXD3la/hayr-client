f1bb2b5e71609a1a289ea418cafb8c05ab21689e
M Jones <jonesman2800@gmail.com>
2020-09-04 17:06:19 -0400

    point endpoint to env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // change endpoint and token_key before deploying client
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api', 
+    API_ENDPOINT: process.env.API_ENDPOINT, 
     TOKEN_KEY: 'hayr-auth-token',
 }
