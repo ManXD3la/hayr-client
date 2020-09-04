ea81eedf6be47e0d516b7b1e7a4594299b8dadc7
M Jones <jonesman2800@gmail.com>
2020-09-04 16:55:39 -0400

    entry in journal page styling

--- src/components/EntryComp/EntryComp.js
+++ src/components/EntryComp/EntryComp.js
@@ -5,13 +5,17 @@ import './EntryComp.css';
 function EntryComp(props) {
     console.log(props.date)
     const moodBorder ={
-        border: `solid 10vh rgb(255,${props.moodPleasant},${props.moodEnergy})`
+        border: `solid 10rem rgb(255,${props.moodPleasant},${props.moodEnergy})`
     }
+
+
+
     return (
         <Link to={`/${props.path}/${props.id}`}>
-            <div className='entryBox' style={moodBorder}
->
-                {!props.date? null: props.date}
+            <div className='entryBox' style={moodBorder}>
+                <div className='entryContent'>
+                    {!props.date? null: props.date}
+                </div>
             </div>
         </Link>
     )
