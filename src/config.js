2384392378c6f7770ba9b1fba8893838a16f5514
M Jones <jonesman2800@gmail.com>
2020-09-06 00:57:54 -0400

    set endpoint for env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // change endpoint and token_key before deploying client
 export default {
-    API_ENDPOINT: process.env.API_ENDPOINT, 
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT, 
     TOKEN_KEY: 'hayr-auth-token',
 }
